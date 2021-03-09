781e3a26d96d07d48463056d05dfa7c14a84ef0c
sepehrabdoli <sepehrskater@yahoo.com>
2021-03-09 10:20:16 +0100

    cookies

--- LoginFormulier.php
+++ LoginFormulier.php
@@ -8,6 +8,8 @@ include("DeleteFunction.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Technolab Leiden | Aanmelden</title>
     <link rel="stylesheet" href="css/style.css">
+
+    <!-- javascript link -->
     <script type="text/javascript" src="SelectCheck.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -38,13 +40,25 @@ include("DeleteFunction.php");
                         <a class="btn btn-outline-success my-2 my-sm-0" href="RegistratieFormulier.php">Registreer</a>   
                     </nav>
            </header>
+
+
+
+
     <section>
-        <form method="POST" id="MainForm">
+        <form method="POST" name="MainForm">
             <h1>Aanmeld formulier</h1>
+
             <div class="form-group d-flex flex-column justify-content-center">
                 <label class="p-1">E-mail:</label>
+
+                <!-- email als naam gebruiken -->
                 <input class="p-1" type="email" id="email" name="email" required>
                 <label class="p-1">Functie:</label>
+
+
+
+
+
                 <select class="p-1" name="functie" id="functie" onchange="yesNoCheck(this);" required>
                     <option></option>
                     <option value="werknemer">Werknemer</option>
@@ -57,7 +71,9 @@ include("DeleteFunction.php");
                     <label class="p-1">Klas:</label>
                     <input class="p-1" type="text" id="klas" name="klas">
                 </div>
-            <button class="SubmitButtonForm" type="submit" name="submit">Verzenden</button>
+
+
+            <button class="SubmitButtonForm"  type="submit" name="submit" value = "Set Cookie" onclick = "WriteCookie()" >Verzenden</button>
         </form>
     </section>
 </body>
--- SelectCheck.js
+++ SelectCheck.js
@@ -1,12 +1,14 @@
-function yesNoCheck(that) {
-    if (that.value == "leraar") {
-        document.getElementById("ifYes").style.display = "block";
-    } else {
-        document.getElementById("ifYes").style.display = "none";
-    }
+
+// hier schrijf je een cookie
+
+function WriteCookie() {
+    var now = new Date();
+    now.setMonth( now.getMonth() + 1 );
+    cookievalue = escape(document.Mainform.email.value) + ";"
+
+    document.cookie = "email=" + cookievalue;
+    document.cookie = "expires=" + now.toUTCString() + ";"
+    document.write ("je bent ingelogt : "  + cookievalue );
 }
 
-if ( window.history.replaceState ) {
-    window.history.replaceState( null, null, window.location.href );
-  }
 
