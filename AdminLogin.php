56d34d5621c5bf01d6616081be9279dc3ae5192a
sepehrabdoli <sepehrskater@yahoo.com>
2021-06-23 15:02:50 +0200

    de value van de form aangepast naar username

--- AdminLogin.php
+++ AdminLogin.php
@@ -35,14 +35,17 @@ include("AdminLoginBackEnd.php");
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-user"></i></span>
                         </div>
-                        <input type="text" class="form-control" name="adminname" value="" placeholder="Gebruikersnaam">
+                        <input type="text" class="form-control" name="username" placeholder="Gebruikersnaam">
+
                     </div>
                     <!-- password -->
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-key"></i></span>
                         </div>
-                        <input type="password" class="form-control" value="" name="password" placeholder="Wachtwoord">
+                        <input type="password" class="form-control"  name="password" placeholder="Wachtwoord">
+
+
                     </div>
                     <div class="form-group">
                         <input type="submit" value="submit" name="submit" class="btn float-right login_btn">
