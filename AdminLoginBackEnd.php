70767864a71caf319cbb77ccc4e1755c7642f051
sepehrabdoli <sepehrskater@yahoo.com>
2021-06-23 15:00:35 +0200

    de code toegevoegd van w3 schhools

--- AdminLoginBackEnd.php
+++ AdminLoginBackEnd.php
@@ -1,41 +1,88 @@
 <?php
-//Dit is het bestand om met de database een connectie te maken
-include("db.php");
+// Initialize the session
+session_start();
 
-function test_input($data) {
-
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
 }
 
+// Include config file
+require_once "db.php";
 
-if ($_SERVER["REQUEST_METHOD"]== "POST") {
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
 
-    $adminname = test_input($_POST["adminname"]);
-    $password = test_input($_POST["password"]);
-    $stmt = $con->prepare("SELECT * FROM admin");
-    $stmt->execute();
-    $users = $stmt->fetchAll();
-//
-//
-//    $hash = password_hash("adminname", PASSWORD_DEFAULT);
-//    var_dump($hash);
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    foreach($users as $user) {
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
 
-        if(($user['adminname'] == $adminname) &&
-            ($user['password'] == $password)) {
-            header("Location: leenformulierInchecken.php");
-        }
-        else {
-            echo "<script language='javascript'>";
-            echo "alert('WRONG INFORMATION')";
-            echo "</script>";
-            die();
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+        if($stmt = mysqli_prepare($con, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
     }
-}
 
-?>
+    // Close connection
+    mysqli_close($con);
+}
